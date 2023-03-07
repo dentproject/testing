a780f5b19d34b7745e2352fc0ae98f81b1b3e857
Kostiantyn Stavruk <kostiantyn.stavruk@plvision.eu>
2023-03-07 20:22:06 +0200

    add l1_port_state_status and l1_link_up_state_software_power_cycle tests
    Signed-off-by: Kostiantyn Stavruk <kostiantyn.stavruk@plvision.eu>

--- DentOS_Framework/DentOsTestbed/src/dent_os_testbed/constants.py
+++ DentOS_Framework/DentOsTestbed/src/dent_os_testbed/constants.py
@@ -73,6 +73,7 @@ PYTEST_SUITES = {
     "suite_functional_vlan": "VLAN functional tests",
     "suite_functional_acl": "Functional ACL tests",
     "suite_functional_qos": "Functional QoS tests",
+    "suite_functional_l1": "Functional L1 tests",
 }
 
 PYTEST_SUITE_GROUPS = {
@@ -111,5 +112,6 @@ PYTEST_SUITE_GROUPS = {
         "suite_functional_bridging",
         "suite_functional_acl",
         "suite_functional_qos",
-        "suite_functional_ipv4"]
+        "suite_functional_ipv4",
+        "suite_functional_l1"]
 }
