c87c75f2153d24f955ed120ebf0849a434d0f0c5
Chetan Murthy <83664998+muchetan@users.noreply.github.com>
2023-03-29 12:34:22 -0700

    handle expection when running network html creation (#219)
    Signed-off-by: Chetan Murthy <muchetan@amazon.com>
    Co-authored-by: MirceaDan <mircea-dan.gheorghe@keysight.com>

--- DentOS_Framework/DentOsTestbed/src/dent_os_testbed/test/test_suite/sanity/test_check_links.py
+++ DentOS_Framework/DentOsTestbed/src/dent_os_testbed/test/test_suite/sanity/test_check_links.py
@@ -76,7 +76,11 @@ async def check_and_validate_switch_links(testbed):
                )
             links_dict[dev.host_name][link] = [links[1], operstate, verified]
 
-    tb_generate_network_diagram(testbed, links_dict)
+    # try to generate a visual version of the network
+    try:
+        tb_generate_network_diagram(testbed, links_dict)
+    except:
+        pass
     all_links_up = True
     for dev, links in links_dict.items():
         for link, links in links.items():
