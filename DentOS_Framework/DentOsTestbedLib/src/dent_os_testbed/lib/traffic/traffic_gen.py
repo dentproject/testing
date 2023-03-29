PLVision/dent-testing

Add support for l1 config update
Signed-off-by: Stepan Vovk <stepan.vovk@plvision.eu>

add speed change

Signed-off-by: Stepan Vovk <stepan.vovk@plvision.eu>

add speed change

Signed-off-by: Stepan Vovk <stepan.vovk@plvision.eu>

yaml fix

Fix 5

Signed-off-by: Stepan Vovk <stepan.vovk@plvision.eu>
Signed-off-by: Serhiy Boiko <serhiy.boiko@plvision.eu>

DentOS_Framework/DentOsTestbedLib/src/dent_os_testbed/lib/traffic/traffic_gen.py
@@ -28,6 +28,7 @@ class TrafficGen(TestLibObject):
             send_ping - [port, dst_ip, src_ip]
             send_arp - [port, src_ip]
             clear_traffic - [traffic_names]
+            update_l1_config - ['speed', 'autoneg', 'ixia_ports']
         
     """
     async def _run_command(api, *argv, **kwarg):
@@ -511,4 +512,26 @@ class TrafficGen(TestLibObject):
         
         """
         return await TrafficGen._run_command("get_drilldown_stats", *argv, **kwarg)
+        
+    async def update_l1_config(*argv, **kwarg):
+        """
+        Platforms: ['ixnetwork']
+        Usage:
+        TrafficGen.update_l1_config(
+            input_data = [{
+                # device 1
+                'dev1' : [{
+                    # command 1
+                        'speed':'string',
+                        'autoneg':'bool',
+                        'ixia_ports':'list',
+                }],
+            }],
+        )
+        Description:
+        - IxiaClient
+          update_l1_config - ['speed', 'autoneg', 'ixia_ports']
+        
+        """
+        return await TrafficGen._run_command("update_l1_config", *argv, **kwarg)
         

dentproject/testing

DentOS_Framework/DentOsTestbedLib/src/dent_os_testbed/lib/traffic/traffic_gen.py
@@ -28,6 +28,7 @@ class TrafficGen(TestLibObject):
             send_ping - [port, dst_ip, src_ip]
             send_arp - [port, src_ip]
             clear_traffic - [traffic_names]
+            update_l1_config - ['speed', 'autoneg', 'tgen_ports']
         
     """
     async def _run_command(api, *argv, **kwarg):
@@ -534,4 +535,27 @@ class TrafficGen(TestLibObject):
         
         """
         return await TrafficGen._run_command("get_drilldown_stats", *argv, **kwarg)
+        
+    async def update_l1_config(*argv, **kwarg):
+        """
+        Platforms: ['ixnetwork']
+        Usage:
+        TrafficGen.update_l1_config(
+            input_data = [{
+                # device 1
+                'dev1' : [{
+                    # command 1
+                        'speed':'string',
+                        'autoneg':'bool',
+                        'tgen_ports':'list',
+                        'duplex':'string',
+                }],
+            }],
+        )
+        Description:
+        - IxiaClient
+          update_l1_config - ['speed', 'autoneg', 'tgen_ports', 'duplex']
+        
+        """
+        return await TrafficGen._run_command("update_l1_config", *argv, **kwarg)
         