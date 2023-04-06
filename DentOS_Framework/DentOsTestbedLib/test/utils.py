7c83f6da27eef587ec869d5140e7e93e7b8267c5
MirceaDan <mircea-dan.gheorghe@keysight.com>
2023-04-06 15:18:35 -0700

    enable flake8 checks (#253)
    auto check  python using flake 8
    fix 3 very long line lengths
    ignore all flake8 failures, to be fixed on by one in subsequent PRs
    
    Signed-off-by: Mircea Dan <mircea-dan.gheorghe@keysight.com>

--- DentOS_Framework/DentOsTestbedLib/test/utils.py
+++ DentOS_Framework/DentOsTestbedLib/test/utils.py
@@ -1,3 +1,4 @@
+import json
 import subprocess
 
 
@@ -19,20 +20,59 @@ class TestDevice:
     async def run_cmd(self, cmd):
         print('\n[' + self.name + '] Running ' + cmd + '\n')
         if 'ip -j address show' in cmd:
-            return (
-                0,
-                """[{"ifindex":1,"ifname":"lo","flags":["LOOPBACK","UP","LOWER_UP"],"mtu":65536,"qdisc":"noqueue","operstate":"UNKNOWN","group":"default","txqlen":1000,"link_type":"loopback","address":"00:00:00:00:00:00","broadcast":"00:00:00:00:00:00","addr_info":[{"family":"inet","local":"127.0.0.1","prefixlen":8,"scope":"host","label":"lo","valid_life_time":4294967295,"preferred_life_time":4294967295},{"family":"inet","local":"10.1.0.1","prefixlen":32,"scope":"global","label":"lo","valid_life_time":4294967295,"preferred_life_time":4294967295},{"family":"inet6","local":"::1","prefixlen":128,"scope":"host","valid_life_time":4294967295,"preferred_life_time":4294967295}]},{},{},{},{},{},{},{},{},{},{},{},{},{}]""",
-            )
+            address_show = [
+                {
+                    'ifindex': 1,
+                    'ifname': 'lo',
+                    'flags': ['LOOPBACK', 'UP', 'LOWER_UP'],
+                    'mtu':65536,
+                    'qdisc':'noqueue',
+                    'operstate':'UNKNOWN',
+                    'group':'default',
+                    'txqlen':1000,
+                    'link_type':'loopback',
+                    'address':'00:00:00:00:00:00',
+                    'broadcast':'00:00:00:00:00:00',
+                    'addr_info':[
+                        {'family': 'inet', 'local': '127.0.0.1', 'prefixlen': 8, 'scope': 'host','label': 'lo', 'valid_life_time': 4294967295, 'preferred_life_time': 4294967295},
+                        {'family': 'inet', 'local': '10.1.0.1', 'prefixlen': 32, 'scope': 'global','label': 'lo', 'valid_life_time': 4294967295, 'preferred_life_time': 4294967295},
+                        {'family': 'inet6', 'local': '::1', 'prefixlen': 128, 'scope': 'host', 'valid_life_time': 4294967295, 'preferred_life_time': 4294967295}
+                    ]
+                },
+                {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}
+            ]
+            return (0, json.dumps(address_show))
+
         if 'ip -j route show' in cmd:
-            return (
-                0,
-                """[{"dst":"default","gateway":"54.240.208.143","dev":"ens1f1","protocol":"bgp","metric":20,"flags":[]}]""",
-            )
+            route_show = [
+                {
+                    'dst': 'default',
+                    'gateway': '54.240.208.143',
+                    'dev': 'ens1f1',
+                    'protocol': 'bgp',
+                    'metric': 20,
+                    'flags': []
+                }
+            ]
+            return (0, json.dumps(route_show))
+
         if 'ip -j link show' in cmd:
-            return (
-                0,
-                """[{"ifindex":1,"ifname":"lo","flags":["LOOPBACK","UP","LOWER_UP"],"mtu":65536,"qdisc":"noqueue","operstate":"UNKNOWN","linkmode":"DEFAULT","group":"default","txqlen":1000,"link_type":"loopback","address":"00:00:00:00:00:00","broadcast":"00:00:00:00:00:00"}]""",
-            )
+            link_show = [
+                {
+                    'ifindex': 1,
+                    'ifname': 'lo',
+                    'flags': ['LOOPBACK', 'UP', 'LOWER_UP'],
+                    'mtu':65536, 'qdisc':'noqueue',
+                    'operstate':'UNKNOWN',
+                    'linkmode':'DEFAULT',
+                    'group':'default',
+                    'txqlen':1000,
+                    'link_type':'loopback',
+                    'address':'00:00:00:00:00:00',
+                    'broadcast':'00:00:00:00:00:00'
+                }
+            ]
+            return (0, json.dumps(link_show))
 
         try:
             process = subprocess.Popen(cmd.split(' '), stdout=subprocess.PIPE)
