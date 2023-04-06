1e5a2635305e89139a8776ce00b32ddd9c9d0601
Anton Putria <anton.putrya@plvision.eu>
2023-04-06 22:57:06 +0300

    Update run tests howto and docker environment (#186)
    - Update docker environment to use the latest DentOS framework code on
      each run
    - Add run.sh script to simplify environment setup
    - Update run tests howto
    
    Signed-off-by: Anton Putria <anton.putrya@plvision.eu>
    Signed-off-by: yuriilisovskyi <yurii.lisovskyi@plvision.eu>

---
+++ DentOS_Framework/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+export DOCKER_BUILDKIT=1
+
+PWD=$(pwd)
+COMMAND="/bin/bash"
+SCRIPT_DIR=$(dirname "$0")
+DOCKERFILE_DIR=$SCRIPT_DIR
+
+# Build base image
+docker build -f "$DOCKERFILE_DIR"/Dockerfile -t dent-testing-base:latest .
+
+# Build working image
+docker build -f "$DOCKERFILE_DIR"/Dockerfile.testing -t dent-testing:latest .
+
+if [ -n "$1" ]; then
+    COMMAND="$*"
+fi
+
+docker run \
+    -ti --rm \
+    --mount=type=bind,target=/DENT/DentOsTestbed,source="$PWD"/DentOsTestbed \
+    --mount=type=bind,target=/DENT/DentOsTestbedDiscovery,source="$PWD"/DentOsTestbedDiscovery \
+    --mount=type=bind,target=/DENT/DentOsTestbedLib,source="$PWD"/DentOsTestbedLib \
+    --name dent-testing-"$USER" \
+    dent-testing:latest \
+    "$COMMAND"
