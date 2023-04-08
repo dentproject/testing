dentproject/testing

commit c4ff89a6d7ab3e6d5b9e887c82f3ef73789c2126

F523 '...'.format(...) has unused arguments at position(s): 1 (#282)
add another {} into the format line, this change looks prone to collaterals, while previous code was wrong it may have worked fine that way

Signed-off-by: Mircea Dan <mircea-dan.gheorghe@keysight.com>

DentOS_Framework/DentOsTestbedLib/src/dent_os_testbed/lib/ethtool/linux/linux_ethtool_impl.py
@@ -55,7 +55,7 @@ class LinuxEthtoolImpl(LinuxEthtool):
         """
         params = kwarg['params']
         # TODO: Implement me
-        cmd = 'ethtool {} '.format(params.get('options', ''), command)
+        cmd = 'ethtool {} {}'.format(params.get('options', ''), command)
         if params.get('query_driver', False):
             cmd += '-i '
         if params.get('statistics', False):
@@ -158,7 +158,7 @@ class LinuxEthtoolImpl(LinuxEthtool):
 
         """
         params = kwarg['params']
-        cmd = 'ethtool -s {} '.format(params.get('options', ''), command)
+        cmd = 'ethtool -s {} {}'.format(params.get('options', ''), command)
         if 'devname' in params:
             cmd += '{} '.format(params['devname'])
         if 'speed' in params:

.pre-commit-config.yaml
@@ -16,4 +16,4 @@ repos:
   rev: 6.0.0
   hooks:
   - id: flake8
-    args: ["--max-line-length=201", "--extend-ignore=W605,F523,F524,F541,F601,F632,F821,F841,E701,E722,E741"]
+    args: ["--max-line-length=201", "--extend-ignore=W605,F524,F541,F601,F632,F821,F841,E701,E722,E741"]

commit 4a0b2d64693fd0054132b31e3b7c607bd9f5fe5f

Fix ethtool impl
`ethtool` does not support `show`/`set` commands, so remove the
`command` argument.
Also make sure that there is a space between cmd arguments.

Fixes: c4ff89a6d7ab3e6d5b9e887c82f3ef73789c2126

Signed-off-by: Serhiy Boiko <serhiy.boiko@plvision.eu>

DentOS_Framework/DentOsTestbedLib/src/dent_os_testbed/lib/ethtool/linux/linux_ethtool_impl.py
@@ -55,7 +55,7 @@ class LinuxEthtoolImpl(LinuxEthtool):
         """
         params = kwarg['params']
         # TODO: Implement me
-        cmd = 'ethtool {} {}'.format(params.get('options', ''), command)
+        cmd = 'ethtool {} '.format(params.get('options', ''))
         if params.get('query_driver', False):
             cmd += '-i '
         if params.get('statistics', False):
@@ -158,7 +158,7 @@ class LinuxEthtoolImpl(LinuxEthtool):
 
         """
         params = kwarg['params']
-        cmd = 'ethtool -s {} {}'.format(params.get('options', ''), command)
+        cmd = 'ethtool -s {} '.format(params.get('options', ''))
         if 'devname' in params:
             cmd += '{} '.format(params['devname'])
         if 'speed' in params: