73b46a62e854cfad9edaef2997aabe14528de75f
Kostiantyn Stavruk <119867993+stavrukPLV@users.noreply.github.com>
2023-05-05 07:24:51 +0300

    Add storm_control tests (#323)
    Signed-off-by: Kostiantyn Stavruk <kostiantyn.stavruk@plvision.eu>

--- DentOS_Framework/DentOsTestbed/src/dent_os_testbed/test/test_suite/functional/storm_control/storm_control_utils.py
+++ DentOS_Framework/DentOsTestbed/src/dent_os_testbed/test/test_suite/functional/storm_control/storm_control_utils.py
@@ -1,4 +1,5 @@
 from dent_os_testbed.lib.devlink.devlink_port import DevlinkPort
+from dent_os_testbed.lib.tc.tc_filter import TcFilter
 
 
 async def devlink_rate_value(dev, name, value, cmode=False, device_host_name=True, set=False, verify=False):
@@ -13,3 +14,35 @@ async def devlink_rate_value(dev, name, value, cmode=False, device_host_name=Tru
         devlink_info = out[0][device_host_name]['parsed_output']
         kbyte_value = devlink_info['param'][dev][0]['values'][0]['value']
         assert kbyte_value == value, f"Verify that storm control rate configured is '{value}' kbps.\n"
+
+
+async def tc_filter_add(dev, vlan_id, src_mac, dst_mac, rate, burst, device_host_name=True):
+    out = await TcFilter.add(
+            input_data=[
+                {
+                    device_host_name: [
+                        {
+                            'dev': dev,
+                            'direction': 'ingress',
+                            'protocol': '0x8100',
+                            'filtertype': {
+                                'skip_sw': '',
+                                'vlan_id': vlan_id,
+                                'src_mac': src_mac,
+                                'dst_mac': dst_mac,
+                            },
+                            'action': {
+                                'trap': '',
+                                'police': {
+                                    'rate': rate,
+                                    'burst': burst,
+                                    'conform-exceed': '',
+                                    'drop': '',
+                                }
+                            },
+                        }
+                    ]
+                }
+            ]
+        )
+    assert out[0][device_host_name]['rc'] == 0, f'Failed to create tc rules.\n{out}'
