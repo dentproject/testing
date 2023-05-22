69861fcfd9686aa984f6cd6df3f38121f1fc1338
Serhiy Boiko <serhiy.boiko@plvision.eu>
2023-05-22 19:28:43 +0300

    Add IPv6 icmp test
    Signed-off-by: Serhiy Boiko <serhiy.boiko@plvision.eu>

--- DentOS_Framework/DentOsTestbed/src/dent_os_testbed/test/test_suite/functional/ipv6/test_ipv6_route.py
+++ DentOS_Framework/DentOsTestbed/src/dent_os_testbed/test/test_suite/functional/ipv6/test_ipv6_route.py
@@ -10,6 +10,7 @@ from dent_os_testbed.utils.test_utils.tgen_utils import (
     tgen_utils_get_dent_devices_with_tgen,
     tgen_utils_traffic_generator_connect,
     tgen_utils_dev_groups_from_config,
+    tgen_utils_clear_traffic_items,
     tgen_utils_get_traffic_stats,
     tgen_utils_setup_streams,
     tgen_utils_start_traffic,
@@ -17,8 +18,13 @@ from dent_os_testbed.utils.test_utils.tgen_utils import (
     tgen_utils_get_loss,
 )
 
+from dent_os_testbed.utils.test_utils.tb_utils import (
+    tb_device_tcpdump,
+)
+
 from dent_os_testbed.test.test_suite.functional.ipv6.ipv6_utils import (
     verify_dut_routes,
+    verify_dut_addrs,
 )
 
 pytestmark = [
@@ -679,3 +685,259 @@ async def test_ipv6_route_metrics(testbed):
         'flags': ['offload', 'rt_offload']})
 
     await verify_dut_routes(dent, connected_routes + expected_routes)
+
+
+async def test_ipv6_icmp(testbed):
+    """
+    Test Name: test_ipv6_icmp
+    Test Suite: suite_functional_ipv6
+    Test Overview:
+        Verify ICMPv6 message types
+    Test Procedure:
+    1. Add IP addresses
+       - Verify IP addresses added, routes created
+    2. Send ICMP echo request from TG
+       - Verify ICMP echo reply is sent from DUT
+    3. Send ICMP echo request from TG with DIP of random IP address in same subnet
+       - Verify ICMP 'host unreachable' message is sent
+    4. Send ICMP echo request from TG with TTL (hop limit) equal to 1
+       - Verify ICMP 'time exceeded' message is sent
+    5. Delete IP from DUT, add a different IP address
+    6. Send ICMP echo request from TG
+       - Verify ICMP message redirect is sent
+    """
+    num_of_ports = 2
+    tgen_dev, dent_devices = await tgen_utils_get_dent_devices_with_tgen(testbed, [], num_of_ports)
+    if not tgen_dev or not dent_devices:
+        pytest.skip('The testbed does not have enough dent with tgen connections')
+    dent_dev = dent_devices[0]
+    dent = dent_dev.host_name
+    tg_ports = tgen_dev.links_dict[dent][0][:num_of_ports]
+    ports = tgen_dev.links_dict[dent][1][:num_of_ports]
+    addr_info = namedtuple('addr_info', ['swp', 'tg', 'swp_ip', 'tg_ip', 'plen'])
+    traffic_duration = 10
+    timeout = 20
+
+    address_map = (
+        addr_info(ports[0], tg_ports[0], '2001:1111::1', '2001:1111::2', 64),
+        addr_info(ports[1], tg_ports[1], '2001:2222::1', '2001:2222::2', 64),
+    )
+
+    out = await IpLink.show(input_data=[{dent: [
+        {'cmd_options': '-j'}
+    ]}], parse_output=True)
+    assert out[0][dent]['rc'] == 0, 'Failed to get port info'
+
+    dut_mac = {link['ifname']: link['address']
+               for link in out[0][dent]['parsed_output']
+               if link['ifname'] in ports}
+
+    # Configure ports up
+    out = await IpLink.set(input_data=[{dent: [
+        {'device': port, 'operstate': 'up'} for port in ports
+    ]}])
+    assert out[0][dent]['rc'] == 0, 'Failed to set port state UP'
+
+    # 1. Add IP addresses
+    out = await IpAddress.add(input_data=[{dent: [
+        {'dev': info.swp, 'prefix': f'{info.swp_ip}/{info.plen}'}
+        for info in address_map
+    ]}])
+    assert out[0][dent]['rc'] == 0, 'Failed to add IP addr to port'
+
+    dev_groups = tgen_utils_dev_groups_from_config(
+        {'ixp': info.tg, 'ip': info.tg_ip, 'gw': info.swp_ip,
+         'plen': info.plen, 'version': 'ipv6'}
+        for info in address_map
+    )
+    await tgen_utils_traffic_generator_connect(tgen_dev, tg_ports, ports, dev_groups)
+
+    # Verify IP addresses added
+    expected_addrs = [
+        {'ifname': info.swp,
+         'should_exist': True,
+         'addr_info': {
+            'family': 'inet6',
+            'local': info.swp_ip,
+            'prefixlen': info.plen}}
+        for info in address_map
+    ]
+    await verify_dut_addrs(dent, expected_addrs)
+
+    # Verify routes created
+    expected_routes = [
+        {'dev': info.swp,
+         'dst': info.swp_ip[:-1] + f'/{info.plen}',
+         'should_exist': True,
+         'flags': ['rt_trap']}
+        for info in address_map
+    ]
+    await verify_dut_routes(dent, expected_routes)
+    # 2. Send ICMP echo request from TG
+    streams = {
+        f'icmpv6 {tg_ports[0]} -> {ports[0]}': {
+            'type': 'raw',
+            'protocol': 'ipv6',
+            'ip_source': dev_groups[tg_ports[0]][0]['name'],
+            'ip_destination': dev_groups[tg_ports[1]][0]['name'],
+            'srcMac': '02:00:00:00:00:01',
+            'dstMac': dut_mac[ports[0]],
+            'srcIp': dev_groups[tg_ports[0]][0]['ip'],
+            'dstIp': address_map[0].swp_ip,
+            'ipproto': 'icmpv6',
+            'icmpType': '128',
+            'icmpCode': '0',
+            'rate': 3,  # pps
+        },
+        f'icmpv6 {tg_ports[1]} -> {ports[1]}': {
+            'type': 'raw',
+            'protocol': 'ipv6',
+            'ip_source': dev_groups[tg_ports[1]][0]['name'],
+            'ip_destination': dev_groups[tg_ports[0]][0]['name'],
+            'srcMac': '02:00:00:00:00:02',
+            'dstMac': dut_mac[ports[1]],
+            'srcIp': dev_groups[tg_ports[1]][0]['ip'],
+            'dstIp': address_map[1].swp_ip,
+            'ipproto': 'icmpv6',
+            'icmpType': '128',
+            'icmpCode': '0',
+            'rate': 3,  # pps
+        },
+    }
+    await tgen_utils_setup_streams(tgen_dev, None, streams)
+
+    tcpdump = [asyncio.create_task(tb_device_tcpdump(dent_dev, info.swp,
+                                                     '-n -c 10 "icmp6 && ip6[40] == 129"',
+                                                     count_only=True, timeout=timeout))
+               for info in address_map]
+
+    await tgen_utils_start_traffic(tgen_dev)
+    await asyncio.sleep(traffic_duration)
+    await tgen_utils_stop_traffic(tgen_dev)
+
+    # Verify ICMP echo reply is sent from DUT
+    captured = await asyncio.gather(*tcpdump)
+    assert all(pkts > 0 for pkts in captured), 'Expected DUT to send ICMPv6 reply'
+
+    # 3. Send ICMP echo request from TG with DIP of random IP address in same subnet
+    streams = {
+        'icmpv6 unknown ip': {
+            'type': 'raw',
+            'protocol': 'ipv6',
+            'ip_source': dev_groups[tg_ports[0]][0]['name'],
+            'ip_destination': dev_groups[tg_ports[1]][0]['name'],
+            'srcMac': '02:00:00:00:00:01',
+            'dstMac': dut_mac[ports[0]],
+            'srcIp': dev_groups[tg_ports[0]][0]['ip'],
+            'dstIp': address_map[1].swp_ip[:-1] + '5',  # 2001:2222::5
+            'ipproto': 'icmpv6',
+            'icmpType': '128',
+            'icmpCode': '0',
+            'rate': 3,  # pps
+        },
+    }
+    await asyncio.sleep(5)  # make sure that traffic is definitely stopped
+    await tgen_utils_clear_traffic_items(tgen_dev)
+    await tgen_utils_setup_streams(tgen_dev, None, streams)
+
+    tcpdump = asyncio.create_task(tb_device_tcpdump(dent_dev, address_map[0].swp,
+                                                    '-n -c 10 "icmp6 && ip6[40] == 1 && ip6[41] == 3"',
+                                                    count_only=True, timeout=timeout))
+
+    await tgen_utils_start_traffic(tgen_dev)
+    await asyncio.sleep(traffic_duration)
+    await tgen_utils_stop_traffic(tgen_dev)
+
+    # Verify ICMP 'host unreachable' message is sent
+    captured = await tcpdump
+    assert captured > 0, 'Expected DUT to send ICMPv6 "host unreachable" reply'
+
+    # 4. Send ICMP echo request from TG with TTL (hop limit) equal to 1
+    streams = {
+        'icmpv6 hop limit 1': {
+            'type': 'raw',
+            'protocol': 'ipv6',
+            'ip_source': dev_groups[tg_ports[0]][0]['name'],
+            'ip_destination': dev_groups[tg_ports[1]][0]['name'],
+            'srcMac': '02:00:00:00:00:01',
+            'dstMac': dut_mac[ports[0]],
+            'srcIp': dev_groups[tg_ports[0]][0]['ip'],
+            'dstIp': dev_groups[tg_ports[1]][0]['ip'],
+            'ipproto': 'icmpv6',
+            'icmpType': '128',
+            'icmpCode': '0',
+            'rate': 3,  # pps
+            'hopLimit': 1,
+        },
+    }
+    await asyncio.sleep(5)
+    await tgen_utils_clear_traffic_items(tgen_dev)
+    await tgen_utils_setup_streams(tgen_dev, None, streams)
+
+    tcpdump = asyncio.create_task(tb_device_tcpdump(dent_dev, address_map[0].swp,
+                                                    '-n -c 10 "icmp6 && ip6[40] == 3 && ip6[41] == 0"',
+                                                    count_only=True, timeout=timeout))
+
+    await tgen_utils_start_traffic(tgen_dev)
+    await asyncio.sleep(traffic_duration)
+    await tgen_utils_stop_traffic(tgen_dev)
+
+    # Verify ICMP 'time exceeded' message is sent
+    captured = await tcpdump
+    assert captured > 0, 'Expected DUT to send ICMPv6 "time exceeded" reply'
+
+    # 5. Delete IP from DUT
+    out = await IpLink.set(input_data=[{dent: [
+        {'device': port, 'operstate': state}
+        for port in ports for state in ['down', 'up']
+    ]}])
+    assert out[0][dent]['rc'] == 0, 'Failed to set port state UP'
+
+    # Add a different IP address
+    address_map = (
+        addr_info(ports[0], tg_ports[0], '2001:1111::1', '2001:1111::2', 64),
+        addr_info(ports[1], tg_ports[1], '2001:1111::f', '2001:1111::1f', 64),
+    )
+    out = await IpAddress.add(input_data=[{dent: [
+        {'dev': info.swp, 'prefix': f'{info.swp_ip}/{info.plen}'}
+        for info in address_map
+    ]}])
+    assert out[0][dent]['rc'] == 0, 'Failed to add IP addr to port'
+
+    dev_groups = tgen_utils_dev_groups_from_config(
+        {'ixp': info.tg, 'ip': info.tg_ip, 'gw': info.swp_ip,
+         'plen': info.plen, 'version': 'ipv6'}
+        for info in address_map
+    )
+    await tgen_utils_traffic_generator_connect(tgen_dev, tg_ports, ports, dev_groups)
+
+    # 6. Send ICMP echo request from TG
+    streams = {
+        f'icmpv6 {tg_ports[0]} -> {tg_ports[1]}': {
+            'type': 'raw',
+            'protocol': 'ipv6',
+            'ip_source': dev_groups[tg_ports[0]][0]['name'],
+            'ip_destination': dev_groups[tg_ports[1]][0]['name'],
+            'srcMac': '02:00:00:00:00:01',
+            'dstMac': dut_mac[ports[0]],
+            'srcIp': dev_groups[tg_ports[0]][0]['ip'],
+            'dstIp': dev_groups[tg_ports[1]][0]['ip'],
+            'ipproto': 'icmpv6',
+            'icmpType': '128',
+            'icmpCode': '0',
+            'rate': 3,  # pps
+        },
+    }
+    await tgen_utils_setup_streams(tgen_dev, None, streams)
+
+    tcpdump = asyncio.create_task(tb_device_tcpdump(dent_dev, address_map[0].swp,
+                                                    '-n -c 10 "icmp6 && ip6[40] == 137"',
+                                                    count_only=True, timeout=timeout))
+
+    await tgen_utils_start_traffic(tgen_dev)
+    await asyncio.sleep(traffic_duration)
+    await tgen_utils_stop_traffic(tgen_dev)
+
+    # Verify ICMP message redirect is sent
+    captured = await tcpdump
+    assert captured > 0, 'Expected DUT to send ICMPv6 "redirect" reply'
